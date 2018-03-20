2d099004b2b7db1c568a354fe7d3a50e2e59b0f8
Kenvin Emiewo <kenvin.emiewo@workforcegroup.com>
2018-03-20 17:35:17 +0100

    added a ScrollView as root element in render function with some dummy scrollable content

--- App.js
+++ App.js
@@ -1,16 +1,75 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View , ScrollView, TouchableHighlight, Image} from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!, it has finally
-        worked i guess you shouldnt come close to developing react native
-        applications on Android if you are not using EXPO else, you probably
-        might Just get frustrated.
-        </Text>
-      </View>
+
+          <ScrollView>
+
+            <View style={styles.iterator}>
+                <Image source={require('./assets/invent.jpg')} />
+                <Text>This is nested in the view Container</Text>
+            </View>
+
+            <View style={styles.blue}>
+                <Text>This is nested in the view Container</Text>
+            </View>
+
+            <View style={styles.red}>
+                <Text>This is nested in the view Container</Text>
+            </View>
+
+            <View style={styles.yellow}>
+                <Text>This is nested in the view Container</Text>
+            </View>
+
+
+            <Text>Open up App.js to start working on your app!</Text>
+            <Text>Open up App.js to start working on your app!</Text>
+            <Text>Open up App.js to start working on your app!</Text>
+            <Text>Open up App.js to start working on your app!</Text>
+            <Text>Open up App.js to start working on your app!</Text>
+            <Text>Open up App.js to start working on your app!</Text>
+            <Text>Open up App.js to start working on your app!</Text>
+            <Text>Open up App.js to start working on your app!</Text>
+            <Text>Open up App.js to start working on your app!</Text>
+            <Text>Open up App.js to start working on your app!</Text>
+            <Text>Open up App.js to start working on your app!</Text>
+            <Text>Open up App.js to start working on your app!</Text>
+            <Text>Open up App.js to start working on your app!</Text>
+            <Text>Open up App.js to start working on your app!</Text>
+            <Text>Open up App.js to start working on your app!</Text>
+            <Text>Open up App.js to start working on your app!</Text>
+            <Text>Open up App.js to start working on your app!</Text>
+            <Text>Open up App.js to start working on your app!</Text>
+            <Text>Open up App.js to start working on your app!</Text>
+            <Text>Open up App.js to start working on your app!</Text>
+
+
+            <Text>Open up App.js to start working on your app!</Text>
+            <Text>Open up App.js to start working on your app!</Text>
+            <Text>Open up App.js to start working on your app!</Text>
+            <Text>Open up App.js to start working on your app!</Text>
+            <Text>Open up App.js to start working on your app!</Text>
+            <Text>Open up App.js to start working on your app!</Text>
+            <Text>Open up App.js to start working on your app!</Text>
+            <Text>Open up App.js to start working on your app!</Text>
+            <Text>Open up App.js to start working on your app!</Text>
+            <Text>Open up App.js to start working on your app!</Text>
+            <Text>Open up App.js to start working on your app!</Text>
+            <Text>Open up App.js to start working on your app!</Text>
+            <Text>Open up App.js to start working on your app!</Text>
+            <Text>Open up App.js to start working on your app!</Text>
+            <Text>Open up App.js to start working on your app!</Text>
+            <Text>Open up App.js to start working on your app!</Text>
+            <Text>Open up App.js to start working on your app!</Text>
+            <Text>Open up App.js to start working on your app!</Text>
+            <Text>Open up App.js to start working on your app!</Text>
+            <Text>Open up App.js to start working on your app!</Text>
+
+
+          </ScrollView>
     );
   }
 }
@@ -18,8 +77,36 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#ff9',
+    backgroundColor: '#00b259',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  iterator: {
+    flex: 1,
+    backgroundColor: '#00b259',
+    alignItems: 'center',
+    height: 100
+  },
+
+  red: {
+    flex: 1,
+    backgroundColor: '#c00',
+    alignItems: 'center',
+    height: 100
+  },
+
+  blue: {
+    flex: 1,
+    backgroundColor: '#039',
+    alignItems: 'center',
+    height: 100
+  },
+
+  yellow: {
+    flex: 1,
+    backgroundColor: '#ff9',
+    alignItems: 'center',
+    height: 100
   },
 });
